d3c0b378da4614c66fc7fc235adfcd9878c85def
Débora Inocêncio <30512134+ABDgame@users.noreply.github.com>
2024-12-05 16:28:15 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ const initGame = () = > {
   let randomObj = words[Math.floor(Math.random() * words.length)]; // getting random object from words
   let wordArray = randomObj.word.split(""); //splitting each letter of random word
   for (let i = wordArray.length -1; i > 0; i--){
-      let j = Math.floor(Math.random() * (i + 1));
+      let j = Math.floor(Math.random() * (i + 1)); //getting random number
+      // shuffling and swiping wordArray letters randomly
+      [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]]
   }
 console.log(wordArray);
 }
