1f5c5b1e59b4ef583ab8ac91403be3dd6b0b5cf6
Débora Inocêncio <30512134+ABDgame@users.noreply.github.com>
2024-12-05 15:48:31 -0300

    Update words.js

--- js/words.js
+++ js/words.js
@@ -1,1 +1,44 @@
-
+const words = [
+  {
+    word: "APITO",
+  },
+  {
+    word: "ANJO",
+  },
+  {
+    word: "AVIÃO",
+  },
+  {
+    word: "ÁRVORE",
+  },
+  {
+    word: "AGULHA",
+  },
+  {
+    word: "ABACAXI",
+  },
+  {
+    word: "ARANHA",
+  },
+  {
+    word: "ANEL",
+  },
+  {
+    word: "ABELHA",
+  },
+  {
+    word: "ABACATE",
+  },
+  {
+    word: "BULE",
+  },
+  {
+    word: "BOI",
+  },
+  {
+    word: "BEBE",
+  },
+  {
+    word: "BOCA",
+  },
+]
