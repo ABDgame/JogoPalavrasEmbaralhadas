6f6d7074e4ad70115614eeda23736282ca177fa8
Débora Inocêncio <30512134+ABDgame@users.noreply.github.com>
2024-12-07 17:39:20 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const wordText = document.querySelector(".word"),
 hintText = document.querySelector(".hint span"),
-timeText = document.querySelector(".hint span"),  
+timeText = document.querySelector(".hint time"),  
 inputField = document.querySelector("input"),  
 refreshBtn = document.querySelector(".refresh-word"),
 checkBtn = document.querySelector(".check-word");
