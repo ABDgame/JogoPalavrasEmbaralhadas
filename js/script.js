8a7074df053aa1bdf38b4aa168c79790f19c6556
Débora Inocêncio <30512134+ABDgame@users.noreply.github.com>
2024-12-07 16:36:02 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const wordText = document.querySelector(".word"),
 hintText = document.querySelector(".hint span"),
 refreshBtn = document.querySelector(".refresh-word"),
-checkBtn = document.querySelector(".refresh-word"),
+checkBtn = document.querySelector(".check-word"),
 
 const initGame = () => {
   let randomObj = words[Math.floor(Math.random() * words.length)]; // getting random object from words
@@ -18,7 +18,7 @@ const initGame = () => {
 initGame();
 
 const checkWord = () => {
-  
+  let userWord = 
 }
 refreshBtn.addEventListener("click", initGame);
 checkBtn.addEventListener("click", checkWord);
