05fd33d0a8393f4ead85bc10ff50434676bb2b5c
Débora Inocêncio <30512134+ABDgame@users.noreply.github.com>
2024-12-07 15:11:23 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ hintText = document.querySelector(".hint span");
 const initGame = () => {
   let randomObj = words[Math.floor(Math.random() * words.length)]; // getting random object from words
   let wordArray = randomObj.word.split(""); //splitting each letter of random word
-  for (let i = wordArray.length -1; i > 0; i--){
+  for (let i = wordArray.length -1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1)); //getting random number
       // shuffling and swiping wordArray letters randomly
       [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
   }
   wordText.innerText = wordArray.join(""); // passing shuffled word as word text
-  hintText.innerText = randomObj.hint
+  hintText.innerText = randomObj.hint; // passing random object hint as hint text
   console.log(wordArray, randomObj);
 }
 initGame();
