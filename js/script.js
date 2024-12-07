e2cd77a43380cc611bc56712a5c43bebab588555
Débora Inocêncio <30512134+ABDgame@users.noreply.github.com>
2024-12-07 15:08:27 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const wordText = document.querySelector(".word"),
 hintText = document.querySelector(".hint span");
 
-const initGame = () = > {
+const initGame = () => {
   let randomObj = words[Math.floor(Math.random() * words.length)]; // getting random object from words
   let wordArray = randomObj.word.split(""); //splitting each letter of random word
   for (let i = wordArray.length -1; i > 0; i--){
